ddbd8f642cf7ea2297d174602bc1ee2d6811698d
asmaamousa <asmaamousa978@gmail.com>
2026-05-02 23:17:50 +0300

    featured book ui

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -13,9 +13,25 @@ class HomeViewBody extends StatelessWidget {
 
       children: [
         CustomAppBar(),
-        ScrolBarItem()
+      FeaturedBookListView()
       ],
     );
   }
 }
 
+class FeaturedBookListView extends StatelessWidget {
+  const FeaturedBookListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+       height: MediaQuery.of(context).size.height*0.25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (index,context){
+      return const ScrolBarItem();
+        }
+        ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/scrol_bar_item.dart
+++ lib/features/home/presentation/views/widgets/scrol_bar_item.dart
@@ -7,13 +7,13 @@ class ScrolBarItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*0.25,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: AspectRatio(
         
         aspectRatio: 3/4,
         child: Container(
-      
+      // padding: EdgeInsets.symmetric(horizontal: 12,vertical: 5),
           decoration:  BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             image:const DecorationImage(
