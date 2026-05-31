f1bd5eed0c24d1ebe9a875d8e10dec55d12a1bf0
asmaamousa <asmaamousa978@gmail.com>
2026-05-31 03:01:37 +0300

    failure class & dartz package

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookora/cores/errors/failure.dart';
+import 'package:bookora/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future <Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future <Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
